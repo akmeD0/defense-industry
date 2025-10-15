730a4bcfe2af4ff91b94d1716d3a46b7e43eb5db
akmeD0 <roma.dovgai1@gmail.com>
2025-10-15 21:47:14 +0300

    User loader change to SQL 2.X

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ login_manager.login_view = "admin"
 
 @login_manager.user_loader
 def load_user(user_id):
-    return User.query.get(int(user_id))
+    return db.session.get(User, int(user_id))
 
 
 # Список користувачів
